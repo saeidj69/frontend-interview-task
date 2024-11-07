2c1689fd9f7a339f8e28e555d38838f5ff481629
farhoodiamirhosein <farhoodiamirhosein@gmail.com>
2024-11-07 12:23:57 +0330

    fix: toast visiblility statement

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <Header />
       <main>
-        {toasts?.length && <Toast />}
+        {toasts?.length ? <Toast /> : null}
         <Router>
           <Routes>
             <Route path="/" element={<Feed />} />
